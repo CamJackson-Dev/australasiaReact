269dd2bd52bb44c534091c348a18d1fb6ad8909b
Cam Jackson <cameroncj86@gmail.com>
2021-03-19 23:59:24 +1000

    Updates

---
+++ precache-manifest.377e1837dd9b23cd2c3c88913c64f75c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f106ee1f5c08df5f28f5cba14ebe1389",
+    "url": "/austest/index.html"
+  },
+  {
+    "revision": "70d89a3bedfd1ced473f",
+    "url": "/austest/static/css/2.0e64f5da.chunk.css"
+  },
+  {
+    "revision": "70d89a3bedfd1ced473f",
+    "url": "/austest/static/js/2.43f3c552.chunk.js"
+  },
+  {
+    "revision": "7dfe6f62f89236eddc7a5e3453aca635",
+    "url": "/austest/static/js/2.43f3c552.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c7cd2e22c434af40dd4c",
+    "url": "/austest/static/js/main.c0305a72.chunk.js"
+  },
+  {
+    "revision": "98099d459201ea2ada06",
+    "url": "/austest/static/js/runtime-main.186ff0cc.js"
+  }
+]);
